9157df969aead652d22fc00e89c8ede1e489566e
bhupeshsahu-06 <bhupeshsahu5699@gmail.com>
2025-05-16 21:38:32 +0530

    change root path

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.use((req,res,next)=>{
     res.send(registeredUser);
 });*/
 
-app.use("/listings",listingRouter);
+app.use("/",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
 
